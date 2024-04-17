cc7992dca4edf51409efdcda0bba33d4b7cb0e53
mfonisodonatuseffiong <kaytwobaba@gmail.com>
2024-04-17 16:23:41 +0000

    0x0B. Python - Input/Output

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python3
 """This module defines a string-to-JSON function"""
 import json
@@ -6,3 +7,4 @@ import json
 def to_json_string(my_obj):
     """Returns the JSON representation of a string object"""
     return json.dumps(my_obj)
+
