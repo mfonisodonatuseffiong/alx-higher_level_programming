a5f1e7ece51ec3b9d483e3b7ba10c874e2be8bb3
mfonisodonatuseffiong <kaytwobaba@gmail.com>
2024-06-22 22:24:58 +0100

    0x0F. Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,30 +1,21 @@
 #!/usr/bin/python3
-'''Define get function'''
+
+
 import MySQLdb
+from sys import argv
 
-
-def main():
-    # 3 arguments take in
-    # make a connection
-    db = MySQLdb.connect(host='localhost',
-                         port=3306,
-                         user=argv[1],
-                         passwd=argv[2],
-                         database=argv[3])
-
-    # create a cursor
-    c = db.cursor()
-
-    # execute query
-    c.execute('SELECT * FROM states ORDER BY id ASC')
-    rows = c.fetchall()
-    for i in rows:
-        print(i)
-
-    # close all cursor and database
-    c.close()
-    db.close()
-
+'''
+a script that lists all states
+from the database
+'''
 if __name__ == "__main__":
-    from sys import argv
-    main()
+    con = MySQLdb.connect(
+        host="localhost", port=3306, user=argv[1],
+        password=argv[2], database=argv[3])
+    cursor = con.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    db = cursor.fetchall()
+    for i in db:
+        print(i)
+    cursor.close()
+    db.close()
