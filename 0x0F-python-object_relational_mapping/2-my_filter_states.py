3ad5228b0d9fcbda6b538e58cbe9edc352293e4b
mfonisodonatuseffiong <kaytwobaba@gmail.com>
2024-06-24 20:08:36 +0100

    0x0F. Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,22 +1,26 @@
 #!/usr/bin/python3
 """Filter states by user input"""
+
 import MySQLdb
+from sys import argv
 
 
 def main():
-
+    """Main function to connect to the database and fetch states by user input"""
     # connect
-    db = MySQLdb.connect(host='localhost',
-                         port=3306,
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3])
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+
     # cursor
     c = db.cursor()
 
     # execute query
-    c.execute("SELECT * FROM states WHERE BINARY name = '{:s}'\
-    ORDER BY id ASC".format(argv[4]))
+    c.execute("SELECT * FROM states WHERE BINARY name = %s ORDER BY id ASC", (argv[4],))
 
     # fetch
     rows = c.fetchall()
@@ -29,6 +33,6 @@ def main():
     c.close()
     db.close()
 
+
 if __name__ == "__main__":
-    from sys import argv
     main()
