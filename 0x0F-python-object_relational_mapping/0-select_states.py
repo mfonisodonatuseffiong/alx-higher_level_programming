6f3b129f136c5de86dd597c5eb5c501a3e16a8c8
mfonisodonatuseffiong <kaytwobaba@gmail.com>
2024-06-24 19:47:41 +0100

    0x0F. Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """
-script that lists all states from the
-database hbtn_0e_0_usa
+Script that lists all states from the
+database hbtn_0e_0_usa.
 """
 
 import MySQLdb
 import sys
+
 if __name__ == '__main__':
     """
     Access to the database
@@ -16,7 +17,8 @@ if __name__ == '__main__':
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3],
-        charset="utf8")
+        charset="utf8"
+    )
 
     cur = conn.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
