b9b15395d665a1e9a772a52447796657b781de4e
mfonisodonatuseffiong <kaytwobaba@gmail.com>
2024-06-25 22:37:37 +0100

    0x0F. Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,29 +1,39 @@
 #!/usr/bin/python3
-# prints the State object with name input
-
+"""Print the State object with the name passed as argument from the database hbtn_0e_6_usa"""
 
 if __name__ == "__main__":
     from model_state import Base, State
     from sys import argv
-    from sqlalchemy.engine.url import URL
     from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
+    from sqlalchemy.orm import sessionmaker
 
-    db = {'drivername': 'mysql+mysqldb',
-          'host': 'localhost',
-          'port': '3306',
-          'username': argv[1],
-          'password': argv[2],
-          'database': argv[3]}
+    if len(argv) != 5:
+        print(f"Usage: {argv[0]} <mysql username> <mysql password> <database name> <state name>")
+        exit(1)
 
-    url = URL(**db)
-    engine = create_engine(url, pool_pre_ping=True)
+    username, password, db_name, state_name = argv[1], argv[2], argv[3], argv[4]
+
+    # Create the database engine
+    engine = create_engine(
+        f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}',
+        pool_pre_ping=True
+    )
     Base.metadata.create_all(engine)
-    session = Session(engine)
 
-    sobj = (argv[4], )
-    try:
-        state = session.query(State).filter(State.name == sobj).one_or_none()
-        print("{}".format(state.id))
-    except:
+    # Create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+
+    # Create a session
+    session = Session()
+
+    # Query for the State object with the specified name
+    state = session.query(State).filter(State.name == state_name).first()
+
+    # Print result
+    if state:
+        print(f"{state.id}")
+    else:
         print("Not found")
+
+    # Close the session
+    session.close()
