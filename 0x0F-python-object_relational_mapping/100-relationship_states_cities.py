f4d092576fa8b98879c437ecac6a3d68063a09fc
mfonisodonatuseffiong <kaytwobaba@gmail.com>
2024-06-25 23:54:30 +0100

    0x0F. Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,29 +1,39 @@
 #!/usr/bin/python3
-"""script to create city and state object"""
+"""Script to create City and State objects"""
 
 if __name__ == "__main__":
-    from sqlalchemy.engine import create_engine
-    from sqlalchemy.engine.url import URL
-    from sqlalchemy.orm import Session
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
     from relationship_city import City
     from relationship_state import Base, State
     from sys import argv
+    from sqlalchemy.engine.url import URL
 
-    db = {'drivername': 'mysql+mysqldb',
-          'host': 'localhost',
-          'port': '3306',
-          'username': argv[1],
-          'password': argv[2],
-          'database': argv[3]}
+    if len(argv) != 4:
+        print(f"Usage: {argv[0]} <mysql username> <mysql password> <database name>")
+        exit(1)
 
-    url = URL(**db)
-    engine = create_engine(url, pool_pre_ping=True)
+    username, password, db_name = argv[1], argv[2], argv[3]
+
+    # Database connection URL
+    db_url = URL(drivername='mysql+mysqldb',
+                 username=username,
+                 password=password,
+                 host='localhost',
+                 port=3306,
+                 database=db_name)
+
+    # Create engine and bind session
+    engine = create_engine(db_url, pool_pre_ping=True)
     Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    session = Session(engine)
+    # Create City and State objects
     city = City(name='San Francisco')
     state = State(name='California', cities=[city])
 
+    # Add objects to session, commit changes, and close session
     session.add(state)
     session.add(city)
     session.commit()
