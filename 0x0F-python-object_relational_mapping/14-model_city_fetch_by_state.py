e8b4e2c366521bba032076a44da5e9ce351ed28d
mfonisodonatuseffiong <kaytwobaba@gmail.com>
2024-06-25 23:45:38 +0100

    0x0F. Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,28 +1,40 @@
 #!/usr/bin/python3
-""" prints all City objects from the database"""
-
+"""Prints all City objects from the database"""
 
 if __name__ == "__main__":
-    from sqlalchemy.engine import create_engine
-    from sqlalchemy.engine.url import URL
+    from sqlalchemy import create_engine
     from sqlalchemy.orm import Session
     from model_state import Base, State
     from model_city import City
     from sys import argv
 
-    db = {'drivername': 'mysql+mysqldb',
-          'host': 'localhost',
-          'port': '3306',
-          'username': argv[1],
-          'password': argv[2],
-          'database': argv[3]}
+    if len(argv) != 4:
+        print(f"Usage: {argv[0]} <mysql username> <mysql password> <database name>")
+        exit(1)
 
-    url = URL(**db)
-    engine = create_engine(url, pool_pre_ping=True)
+    username, password, db_name = argv[1], argv[2], argv[3]
+
+    # Create the database engine
+    engine = create_engine(
+        f'mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}',
+        pool_pre_ping=True
+    )
     Base.metadata.create_all(engine)
 
-    session = Session(engine)
-    for state, city in session.query(State, City)\
-                              .filter(State.id == City.state_id).all():
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    # Create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+
+    # Create a session
+    session = Session()
+
+    # Query all City objects joined with State and sorted by city id
+    query = session.query(State.name, City)\
+                   .filter(State.id == City.state_id)\
+                   .order_by(City.id).all()
+
+    # Print results
+    for state_name, city in query:
+        print(f"{state_name}: ({city.id}) {city.name}")
+
+    # Close the session
     session.close()
