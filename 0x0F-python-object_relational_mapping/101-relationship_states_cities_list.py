a3efe388303246dfafb9fd9f87411923b2fabda6
mfonisodonatuseffiong <kaytwobaba@gmail.com>
2024-06-27 00:05:27 +0100

    0x0F. Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
-"""script to create city and state object"""
+"""Script to create City and State objects"""
 
 if __name__ == "__main__":
-    from sqlalchemy.engine import create_engine
-    from sqlalchemy.engine.url import URL
+    from sqlalchemy import create_engine
     from sqlalchemy.orm import Session
+    from sqlalchemy.engine.url import URL
     from relationship_city import City
     from relationship_state import Base, State
     from sys import argv
 
-    db = {'drivername': 'mysql+mysqldb',
-          'host': 'localhost',
-          'port': '3306',
-          'username': argv[1],
-          'password': argv[2],
-          'database': argv[3]}
+    db = {
+        'drivername': 'mysql+mysqldb',
+        'host': 'localhost',
+        'port': '3306',
+        'username': argv[1],
+        'password': argv[2],
+        'database': argv[3]
+    }
 
     url = URL(**db)
     engine = create_engine(url, pool_pre_ping=True)
--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
-"""script to create city and state object"""
+"""Script to list all City objects from the database"""
 
 if __name__ == "__main__":
-    from sqlalchemy.engine import create_engine
-    from sqlalchemy.engine.url import URL
+    from sqlalchemy import create_engine
     from sqlalchemy.orm import Session
+    from sqlalchemy.engine.url import URL
     from relationship_city import City
     from relationship_state import Base, State
     from sys import argv
 
-    db = {'drivername': 'mysql+mysqldb',
-          'host': 'localhost',
-          'port': '3306',
-          'username': argv[1],
-          'password': argv[2],
-          'database': argv[3]}
+    db = {
+        'drivername': 'mysql+mysqldb',
+        'host': 'localhost',
+        'port': '3306',
+        'username': argv[1],
+        'password': argv[2],
+        'database': argv[3]
+    }
 
     url = URL(**db)
     engine = create_engine(url, pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     session = Session(engine)
-    for city, state in session.query(City, State)\
-                              .filter(City.state_id == State.id):
-        print("{}: {} -> {}".format(city.id, city.name, state.name))
+    for city in session.query(City).order_by(City.id).all():
+        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
 
     session.close()
