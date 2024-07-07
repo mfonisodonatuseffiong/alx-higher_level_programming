d3c2f6f2fb36c923115dc0fe77f11c2c41382f4a
mfonisodonatuseffiong <kaytwobaba@gmail.com>
2024-07-07 10:11:47 +0100

    0x11. Python - Network #1

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python3
-"""Sends a POST request to http://0.0.0.0:5000/search_user with a given letter.
+"""Send a POST request to http://0.0.0.0:5000/search_user with a given letter.
 Usage: ./8-json_api.py <letter>
-  - The letter is sent as the value of the variable `q`.
-  - If no letter is provided, sends `q=""`.
+  - The letter must be sent in the variable q.
+  - If no argument is given, set q="".
+  - If the response body is properly JSON formatted and not empty,
+    display the id and name like this: [<id>] <name>.
+  - Otherwise:
+    - Display Not a valid JSON if the JSON is invalid.
+    - Display No result if the JSON is empty.
 """
 import sys
 import requests
 
-
 if __name__ == "__main__":
-    letter = "" if len(sys.argv) == 1 else sys.argv[1]
-    payload = {"q": letter}
+    url = "http://0.0.0.0:5000/search_user"
+    letter = sys.argv[1] if len(sys.argv) > 1 else ""
+    payload = {'q': letter}
 
-    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
     try:
-        response = r.json()
-        if response == {}:
-            print("No result")
+        response = requests.post(url, data=payload)
+        response_json = response.json()
+        
+        if response_json:
+            print(f"[{response_json.get('id')}] {response_json.get('name')}")
         else:
-            print("[{}] {}".format(response.get("id"), response.get("name")))
+            print("No result")
     except ValueError:
         print("Not a valid JSON")
