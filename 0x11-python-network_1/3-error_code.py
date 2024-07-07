b249dc74618a0353d9a0a0f2133f6840b4b48c6e
mfonisodonatuseffiong <kaytwobaba@gmail.com>
2024-07-07 09:42:17 +0100

    0x11. Python - Network #1

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,6 +2,7 @@
 """Send a request to a given URL and display the body of the response.
 Usage: ./3-error_code.py <URL>
   - Manage urllib.error.HTTPError exceptions and print: Error code: followed by the HTTP status code.
+  - Manage urllib.error.URLError exceptions and print: Error: <error message>.
 """
 import sys
 import urllib.request
@@ -15,3 +16,5 @@ if __name__ == "__main__":
             print(response.read().decode('utf-8'))
     except urllib.error.HTTPError as e:
         print(f"Error code: {e.code}")
+    except urllib.error.URLError as e:
+        print(f"Error: {e.reason}")
