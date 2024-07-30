1a860e6e54b8b827ee997c9d6ad1ac036bd8dad0
mfonisodonatuseffiong <kaytwobaba@gmail.com>
2024-07-30 12:08:23 +0100

    0x14. JavaScript - Web scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,22 @@
 #!/usr/bin/node
 
-let fs = require('fs');
+const fs = require('fs');
 
-fs.readFile(process.argv[2], function (err, buf) {
-  if (!err) {
-    console.log(buf.toString());
+// Get the file path from the command line arguments
+const filePath = process.argv[2];
+
+if (!filePath) {
+  console.error('Error: No file path provided.');
+  process.exit(1); // Exit the script with a non-zero code to indicate an error
+}
+
+// Read the file content
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    // Print the error object if an error occurred
+    console.error(err);
   } else {
-    console.log(err);
+    // Print the file content
+    console.log(data);
   }
 });
