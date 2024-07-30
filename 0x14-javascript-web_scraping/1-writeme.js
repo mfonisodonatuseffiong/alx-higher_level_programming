7874ed0031f8ec82beefac9b788bdc8372178703
mfonisodonatuseffiong <kaytwobaba@gmail.com>
2024-07-30 12:15:43 +0100

    0x14. JavaScript - Web scraping

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,7 +4,12 @@ const fs = require('fs');
 const filename = process.argv[2];
 const content = process.argv[3];
 
-fs.writeFile(filename, content, 'utf-8', (error) => {
+if (!filename || !content) {
+  console.error('Error: Missing file path or content.');
+  process.exit(1); // Exit the script with a non-zero code to indicate an error
+}
+
+fs.writeFile(filename, content, 'utf8', (error) => {
   if (error) {
     console.log(error);
   }
