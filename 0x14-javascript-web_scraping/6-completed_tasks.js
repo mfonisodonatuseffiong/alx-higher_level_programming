39a378d566965b06634a50fc649638db1b61fe75
mfonisodonatuseffiong <kaytwobaba@gmail.com>
2024-07-30 13:10:20 +0100

    0x14. JavaScript - Web scraping

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  try {
+    const todos = JSON.parse(body);
+    const completedTasks = {};
+
+    todos.forEach(todo => {
+      if (todo.completed) {
+        if (!completedTasks[todo.userId]) {
+          completedTasks[todo.userId] = 0;
+        }
+        completedTasks[todo.userId]++;
+      }
+    });
+
+    console.log(completedTasks);
+  } catch (e) {
+    console.error('Error parsing JSON:', e);
+  }
+});
